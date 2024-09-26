9088a55cfeee381ada21a9a98763c6813c63e2e5
TimonKosgei <timonkosgei15209@gmail.com>
2024-09-26 15:59:30 +0300

    Did labs

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+
+function superbowlWin(records){
+    let won= records.find(obj => obj.result === "W")
+    if(won !== undefined){
+        return won.year;
+    }else{
+        return undefined;
+    }
+}
